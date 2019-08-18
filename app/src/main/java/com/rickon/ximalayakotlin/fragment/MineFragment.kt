e35a8a71493497b44f89c84ed3ee8916779647f2
gs666 <gaoshuo2016@qq.com>
2019-08-18 14:50:53 +0800

    feat:完善我的内容,增加最近收听跳转

--- app/src/main/java/com/rickon/ximalayakotlin/fragment/MineFragment.kt
+++ app/src/main/java/com/rickon/ximalayakotlin/fragment/MineFragment.kt
@@ -1,6 +1,7 @@
 package com.rickon.ximalayakotlin.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,8 +11,10 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.rickon.ximalayakotlin.R
+import com.rickon.ximalayakotlin.activities.HistoryActivity
+import kotlinx.android.synthetic.main.fragment_mine_layout.*
 
-class MineFragment : BaseFragment() {
+class MineFragment : BaseFragment(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,36 @@ class MineFragment : BaseFragment() {
         // Inflate the layout for this fragment
         Log.d(TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_mine_layout, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        recent_listen_btn.setOnClickListener(this)
+        my_download_btn.setOnClickListener(this)
+        my_like_btn.setOnClickListener(this)
+        my_setting_btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.recent_listen_btn -> {
+                val intent = Intent(mContext, HistoryActivity::class.java)
+                this.startActivity(intent)
+            }
+            R.id.my_download_btn -> {
+
+            }
+            R.id.my_like_btn -> {
+
+            }
+            R.id.my_setting_btn -> {
+
+            }
+        }
     }
 
     override fun onDestroy() {
